7117eb085577746b1e1f6b9cddab9bf9e1f57859
abigmiu <suraelax@gmail.com>
2022-09-18 15:42:51 +0800

    关联 key 与 effectFn

--- proxy.js
+++ proxy.js
@@ -1,26 +1,51 @@
-const bucker = new Set();
+const bucker = new WeakMap();
 const data = {
     text: 'hello world',
 };
 
+let activeEffect;
+function effect(fn) {
+    activeEffect = fn;
+    fn();
+}
+
 const obj = new Proxy(data, {
     get(target, key) {
-        bucker.add(effect);
+        if (!activeEffect) return target[key];
+
+        let depsMap = bucker.get(target);
+        if (!depsMap) {
+            depsMap = new Map();
+            bucker.set(target, depsMap);
+        }
+
+        let deps = depsMap.get(key);
+        if (!deps) {
+            deps = new Set();
+            depsMap.set(key, deps);
+        }
+
+        deps.add(activeEffect);
+
         return target[key];
     },
     set(target, key, newVal) {
         target[key] = newVal;
-        bucker.forEach((fn) => fn());
-        return true;
+
+        const depsMap = bucker.get(target);
+        if (!depsMap) return;
+
+        const effects = depsMap.get(key);
+
+        effects && effects.forEach((fn) => fn());
     },
 });
 
-function effect() {
+effect(() => {
+    console.log('effect dn');
     document.body.innerText = obj.text;
-}
-
-effect();
+});
 
 setTimeout(() => {
-    obj.text = 'hello vue3';
+    obj.noExitProperty = 'hello vue3';
 }, 1000);

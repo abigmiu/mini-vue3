239b6daa13199902b8de70501eac18e6d3e1eb93
abigmiu <suraelax@gmail.com>
2022-10-30 21:27:52 +0800

    连续多次修改响应式数据，只触发一次更新

--- core/reactivity/effect.test.js
+++ core/reactivity/effect.test.js
@@ -8,7 +8,7 @@ describe('effect', () => {
         const counter = reactive({
             num: 0,
         });
-        effect(() => dummy = counter.num + 1);
+        effect(() => (dummy = counter.num + 1));
 
         expect(dummy).toBe(1);
         counter.num = 7;
@@ -19,7 +19,7 @@ describe('effect', () => {
         let text;
         const obj = reactive({
             ok: true,
-            text: 'hello'
+            text: 'hello',
         });
 
         const fn = vitest.fn(() => {
@@ -69,32 +69,74 @@ describe('effect', () => {
         expect(fn2).toHaveBeenCalledTimes(3);
     });
 
-    it('避免无限递归循环', function() {
-        const obj = reactive({foo: 1})
+    it('避免无限递归循环', function () {
+        const obj = reactive({ foo: 1 });
         effect(() => {
             // 同时 get + set -> 在运行当前 effect 未结束时，又调用了当前 effect
-            obj.foo++
-        })
+            obj.foo++;
+        });
     });
 });
 
 describe('scheduler', () => {
-    it('使用 scheduler 控制调度时机', function() {
-        vitest.useFakeTimers()
-        const obj = reactive({foo: 1})
-        let bar
+    it('使用 scheduler 控制调度时机', function () {
+        vitest.useFakeTimers();
+        const obj = reactive({ foo: 1 });
+        let bar;
 
-        effect(() => {
-            bar = obj.foo
-        }, {
-            scheduler(fn) {
-                setTimeout(fn)
+        effect(
+            () => {
+                bar = obj.foo;
+            },
+            {
+                scheduler(fn) {
+                    setTimeout(fn);
+                },
             }
-        })
-        obj.foo = 0
+        );
+        obj.foo = 0;
 
-        bar = 3
-        vitest.runAllTimers()
-        expect(bar).toBe(0)
+        bar = 3;
+        vitest.runAllTimers();
+        expect(bar).toBe(0);
+    });
+
+    it('联系多次修改响应式数据， 只触发一次更新', async function () {
+        const p = Promise.resolve();
+        const jobQueue = new Set();
+
+        let isFlushing = false;
+        function flushJob() {
+            if (isFlushing) return;
+
+            isFlushing = true;
+
+            p.then(() => {
+                jobQueue.forEach((job) => job());
+            }).finally(() => {
+                isFlushing = false;
+            });
+        }
+
+        const obj = reactive({
+            foo: 1,
+        });
+        const fn = vitest.fn(() => console.log(obj.foo));
+        effect(fn, {
+            scheduler(fn) {
+                jobQueue.add(fn);
+                flushJob();
+            },
+        });
+
+        obj.foo++;
+        obj.foo++;
+        obj.foo++;
+        obj.foo++;
+
+        await Promise.resolve;
+
+        expect(fn).toHaveBeenCalledTimes(2);
+        expect(obj.foo).toBe(5);
     });
 });
